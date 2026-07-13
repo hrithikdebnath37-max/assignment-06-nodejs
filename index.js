5bfba4883569e0d55199bf77808301c0a2d480a3
Debnath <hrithikdebnath37@gmail.com>
2026-07-13 20:56:26 +0600

    Add Home Route

--- index.js
+++ index.js
@@ -2,11 +2,46 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
 
-    res.writeHead(200, {
-        "Content-Type": "text/plain"
-    });
+    if (req.url === "/") {
 
-    res.end("Server is Running");
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+
+        res.end(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Home</title>
+            </head>
+            <body>
+                <h1>Home Page</h1>
+                <p>Welcome to our Home Page.</p>
+            </body>
+            </html>
+        `);
+
+    }
+
+    else {
+
+        res.writeHead(404, {
+            "Content-Type": "text/html"
+        });
+
+        res.end(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>404</title>
+            </head>
+            <body>
+                <h1>404 - Page Not Found</h1>
+            </body>
+            </html>
+        `);
+
+    }
 
 });
 
