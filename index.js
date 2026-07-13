911ceaf29eb81d07bae0e73a2d3867d466f3e225
Debnath <hrithikdebnath37@gmail.com>
2026-07-13 20:35:45 +0600

    Create HTTP Server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const http = require("http");
+
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+
+    res.end("Server is Running");
+
+});
+
+server.listen(5000, () => {
+    console.log("Server Running at http://localhost:5000");
+});
